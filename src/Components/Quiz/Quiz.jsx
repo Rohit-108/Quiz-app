75fbc4bb1361efbdea1483b9ca4c4d84b91a958f
Rohit-108 <rohitsharma1082001@gmail.com>
2024-07-13 22:47:43 +0530

    initial commit

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -1,24 +1,31 @@
-import React from 'react';
+
+import {data} from '../../assets/data';
 import "./Quiz.css";
+import { useState } from "react";
 
 const Quiz = () => {
+
+    let [index,setIndex] = useState(0)
+    let [question,setQuestion] = useState(data[index]);
+
+
   return (
-    <div className='container'>
-        <h1>Quiz App</h1>
-        <hr />
-        <h2>Which device is required for the internet Connection?</h2>
-        <ul>
-            <li>Modem</li>
-            <li>Router</li>
-            <li>Lan Cable</li>
-            <li>Pen Drive</li>
-
-        </ul>
-        <button>Next</button>
-        <div className='index'>1 of 5 questions</div>
-
+    <div className="container">
+      <h1>Quiz App</h1>
+      <hr />
+      <h2>
+        {index + 1}.{question.question}
+      </h2>
+      <ul>
+        <li>{question.option1}</li>
+        <li>{question.option2}</li>
+        <li>{question.option2}</li>
+        <li>{question.option2}</li>
+      </ul>
+      <button>Next</button>
+      <div className="index">1 of 5 questions</div>
     </div>
-  )
+  );
 }
 
 export default Quiz
---
+++ src/assets/data.js
@@ -0,0 +1,45 @@
+export const data = {
+    {
+        question: "Which device is required for the internet Connection?",
+        option1:"Modem",
+        option2:"Router",
+        option3:"LAN Cable",
+        option4:"Pen Drive",
+        ans:1,
+    },
+
+    {
+        question: "Which continent has the highest number of countries",
+        option1:"Asia",
+        option2:"Europe",
+        option3:"North America",
+        option4:"Africa",
+        ans:4,
+    },
+{
+question: "Junk e-mail is also called?",
+  option1:"Spam",
+  option2:"fake",
+  option3:"Archive",
+  option4:"Bin",
+  ans:1,
+},
+{
+question: "A computer cannot Boot if it does not have the?",
+  option1:"Application Software",
+  option2:"Internet",
+  option3:"operating System",
+  option4:"Mouse",
+  ans:3,
+},
+
+{
+question: "First page of website is termed as?",
+  option1:"Index page",
+  option2:"Home Page",
+  option3:"Sitemap",
+  option4:"Pen Drive",
+  ans:2,
+}
+
+};
